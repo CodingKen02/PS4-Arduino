06e46813fe7482e3ed0da04d4f06b116a0626d26
Kennedy <71536021+CodingKen02@users.noreply.github.com>
2023-11-15 01:42:34 -0600

    Update PS4USB_WheelControl.ino
    adding reference

--- PS4USB_WheelControl.ino
+++ PS4USB_WheelControl.ino
@@ -1,4 +1,10 @@
-// Turning Wheels - kennedy
+/*
+ Example sketch for the PS4 USB library - developed by Kristian Lauszus
+ For more information visit my blog: http://blog.tkjelectronics.dk/ or
+ send me an e-mail:  kristianl@tkjelectronics.com
+ */
+
+// Turning Wheels - kennedy | Reference Above
 
 #include <PS4USB.h>
 
