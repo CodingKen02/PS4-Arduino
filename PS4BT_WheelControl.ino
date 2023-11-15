326eb62173cdc109434b91346b4865ee41cfec65
Kennedy <71536021+CodingKen02@users.noreply.github.com>
2023-11-15 01:43:57 -0600

    Update PS4BT_WheelControl.ino
    adding reference

--- PS4BT_WheelControl.ino
+++ PS4BT_WheelControl.ino
@@ -1,5 +1,5 @@
 /*
- Example sketch for the PS4 USB library - developed by Kristian Lauszus
+ Example sketch for the PS4 Bluetooth library - developed by Kristian Lauszus
  For more information visit my blog: http://blog.tkjelectronics.dk/ or
  send me an e-mail:  kristianl@tkjelectronics.com
  */
